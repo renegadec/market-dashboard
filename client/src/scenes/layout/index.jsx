7f708a33462bd5f7f31f847d248053d5163cc36f
africoiner <confidencenyirenda7@gmail.com>
2023-02-27 13:21:42 +0200

    added user profile and logout on sidebar and navbar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -11,9 +11,7 @@ const Layout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const userId = useSelector((state) => state.global.userId);
     const { data } = useGetUserQuery(userId);
-    console.log("data:", data);
     
-
     return (
         <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
             <Sidebar
@@ -23,7 +21,7 @@ const Layout = () => {
                 isSidebarOpen={isSidebarOpen}
                 setIsSidebarOpen={setIsSidebarOpen} 
             />
-            <Box>
+            <Box flexGrow={1}>
                 <Navbar 
                     user={ data || {} }
                     isSidebarOpen={isSidebarOpen}

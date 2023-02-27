a5caa18e5b9c871127fdeafc95c1c6689b5da283
africoiner <confidencenyirenda7@gmail.com>
2023-02-27 10:24:32 +0200

    added sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,12 +3,35 @@ import { Box, useMediaQuery } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/SideBar";
+import { useGetUserQuery } from '../../state/api';
 
 const Layout = () => {
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const userId = useSelector((state) => state.global.userId);
+    const { data } = useGetUserQuery(userId);
+    console.log("data:", data);
+    
+
     return (
-        <Box width="100%" height="100%">
-            <Navbar />
-            <Outlet />
+        <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+            <Sidebar
+                user={ data || {} }
+                isNonMobile={isNonMobile}
+                drawerWidth="250px"
+                isSidebarOpen={isSidebarOpen}
+                setIsSidebarOpen={setIsSidebarOpen} 
+            />
+            <Box>
+                <Navbar 
+                    user={ data || {} }
+                    isSidebarOpen={isSidebarOpen}
+                    setIsSidebarOpen={setIsSidebarOpen}
+                />
+                <Outlet />
+            </Box>
+            
         </Box>
     )
 }
